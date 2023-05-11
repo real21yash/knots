93a781cd608c1070d33f7858aac5b9bb360f463f
real21yash <131291578+real21yash@users.noreply.github.com>
2023-05-11 11:55:19 +0530

    Add files via upload

---
+++ Knots/Form1.cs
@@ -0,0 +1,251 @@
+﻿//Made by real21yash, Knots 1.0
+using System;
+using System.Windows.Forms;
+
+namespace Knots
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Set the initial home page when the form is loaded
+            webBrowser1.Navigate("https://www.duckduckgo.com");
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            // Navigate to the URL entered in the address bar
+            webBrowser1.Navigate(txtAddress.Text);
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            // Update the address bar with the current URL after navigation
+            txtAddress.Text = webBrowser1.Url.ToString();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            // Navigate back to the previous page
+            if (webBrowser1.CanGoBack)
+            {
+                webBrowser1.GoBack();
+            }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            // Navigate forward to the next page
+            if (webBrowser1.CanGoForward)
+            {
+                webBrowser1.GoForward();
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            // Refresh the current page
+            webBrowser1.Refresh();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            // Stop loading the current page
+            webBrowser1.Stop();
+        }
+
+        private void txtAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Navigate to the URL entered in the address bar when Enter key is pressed
+            if (e.KeyCode == Keys.Enter)
+            {
+                webBrowser1.Navigate(txtAddress.Text);
+                e.SuppressKeyPress = true; // Prevent default behavior of Enter key
+            }
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            // Navigate to the home page
+            webBrowser1.Navigate("https://www.duckduckgo.com");
+        }
+
+        private void btnFavorites_Click(object sender, EventArgs e)
+        {
+            // Show a form to manage favorites/bookmarks
+            Form favoritesForm = new Form();
+
+            favoritesForm.ShowDialog();
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnFavorites = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(174, 28);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(1315, 22);
+            this.txtAddress.TabIndex = 1;
+            this.txtAddress.TextChanged += new System.EventHandler(this.txtAddress_TextChanged);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(12, 28);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(93, 57);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Enabled = false;
+            this.btnBack.Location = new System.Drawing.Point(93, 28);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 4;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(1414, 57);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Enabled = false;
+            this.btnForward.Location = new System.Drawing.Point(12, 57);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(75, 23);
+            this.btnForward.TabIndex = 6;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            // 
+            // btnFavorites
+            // 
+            this.btnFavorites.Location = new System.Drawing.Point(1495, 57);
+            this.btnFavorites.Name = "btnFavorites";
+            this.btnFavorites.Size = new System.Drawing.Size(75, 23);
+            this.btnFavorites.TabIndex = 7;
+            this.btnFavorites.Text = "Favorites";
+            this.btnFavorites.UseVisualStyleBackColor = true;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(1495, 28);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(75, 23);
+            this.btnHome.TabIndex = 8;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 86);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1558, 655);
+            this.webBrowser1.TabIndex = 9;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted_1);
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(1582, 753);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.btnFavorites);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtAddress);
+            this.Name = "MainForm";
+            this.Text = "Knots";
+            this.Load += new System.EventHandler(this.MainForm_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private TextBox txtAddress;
+        private Button btnGo;
+        private Button btnRefresh;
+        private Button btnBack;
+        private Button btnStop;
+        private Button btnForward;
+        private Button btnFavorites;
+        private Button btnHome;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAddress_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private WebBrowser webBrowser1;
+
+        private void webBrowser1_DocumentCompleted_1(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
